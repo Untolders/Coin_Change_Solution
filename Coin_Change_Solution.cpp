c59ee054241cc587266e65e35eee2fc2f32f94e0
Anurag Singh <131255604+Untolders@users.noreply.github.com>
2023-08-11 13:46:31 +0530

    Create Coin_Change_Solution.cpp

---
+++ Coin_Change_Solution.cpp
@@ -0,0 +1,88 @@
+************************************************************************  Question  ******************************************************************************
+Given an integer array coins[ ] of size N representing different denominations of currency and an integer sum, find the number of ways you can make sum by using different combinations from coins[ ].  
+Note: Assume that you have an infinite supply of each type of coin. And you can use any coin as many times as you want.
+
+Example 1:
+
+Input:
+N = 3, sum = 4
+coins = {1,2,3}
+Output: 4
+Explanation: Four Possible ways are: {1,1,1,1},{1,1,2},{2,2},{1,3}.
+Example 2:
+
+Input:
+N = 4, Sum = 10
+coins = {2,5,3,6}
+Output: 5
+Explanation: Five Possible ways are: {2,2,2,2,2}, {2,2,3,3}, {2,2,6}, {2,3,5} and {5,5}.
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function count() which accepts an array coins its size N and sum as input parameters and returns the number of ways to make change for given sum of money. 
+
+Expected Time Complexity: O(sum*N)
+Expected Auxiliary Space: O(sum)
+
+Constraints:
+1 <= sum, N, coins[i] <= 103
+
+************************************************************************  Solution  ******************************************************************************  
+
+  //{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+  
+long long n;
+    long long t[1001][1001];
+    long long solve(int idx,int sum,int coins[]){
+        if(sum==0){
+            return 1;
+        }
+        if(idx==n){
+            return 0;
+        }
+        if(t[idx][sum]!=-1){
+            return t[idx][sum];
+        }
+        if(sum<coins[idx]){
+           
+            return t[idx][sum]=solve(idx+1,sum,coins);
+        }
+        
+        long long take=solve(idx,sum-coins[idx],coins);
+       
+       
+        long long skip=solve(idx+1,sum,coins);
+        
+        return t[idx][sum] =take+skip;
+        
+    }
+    long long int count(int coins[], int N, int sum) {
+
+        n=N;
+        memset(t,-1,sizeof(t));
+        return solve(0,sum,coins);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int sum, N;
+        cin >> sum >> N;
+        int coins[N];
+        for (int i = 0; i < N; i++) cin >> coins[i];
+        Solution ob;
+        cout << ob.count(coins, N, sum) << endl;
+    }
+
+    return 0;
+}
+
+
+// } Driver Code Ends
